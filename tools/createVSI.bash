36c19d09fc40e245aba2d1203e610d39bbf57805
andrewj2000 <andrewj@us.ibm.com>
2023-10-03 07:00:57 -0500

    update bash script to handle periods in user names

--- tools/createVSI.bash
+++ tools/createVSI.bash
@@ -4,8 +4,10 @@
 #
 echo "Retrieving IAM token..."
 export iam_token=`ibmcloud iam oauth-tokens |cut -f 5 -d' '`
-# Get user id to create vsi name, convert to lower case
-export userName=`id -un`
+# Get user id to create vsi name, change any .'s to -'s since VPC VSIs can't contain periods 
+#export userName=`id -un`
+export userName=`id -un|tr '.' '-'`
+# convert to lower case and add unique extension so I know which VSIs came from our lab
 export vsiName="${userName,,}"-$RANDOM-scc-vsi
 echo "Creating VSI named: $vsiName ..."
 echo ""
